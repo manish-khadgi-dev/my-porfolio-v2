43b8e28f24d0b308b952e5433d5f0a846034dc93
moonishkale <manishkhadgi123@gmail.com>
2023-04-20 18:47:53 +1000

    services added

--- src/components/Services.js
+++ src/components/Services.js
@@ -31,14 +31,16 @@ const Services = () => {
   return (
     <section className="section" id="services">
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col lg:flex-row">
           {/* {text} */}
-          <div>
-            <h2 className="h2 text-accent"> What I Do.</h2>
-            <h3 className="h3 max-w-[455px">
+          <div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0 ">
+            <h2 className="h2 text-accent mb-6"> What I Do.</h2>
+            <h3 className="h3 max-w-[455px mb-16">
               I'm a Freelance Full-stack-developer with experience in multiple
               projects
             </h3>
+
+            <button className="btm btn-sm"> See My Work </button>
           </div>
 
           {/* { services } */}
